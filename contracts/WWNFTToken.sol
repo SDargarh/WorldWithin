1da22ba18886fca014c5373687a7465742bd88b2
SDargarh <sdargarh26@gmail.com>
2021-08-09 12:55:05 +0530

    counter increment correction for minting token

--- contracts/WWNFTToken.sol
+++ contracts/WWNFTToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.7;
 
-import "../client/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import "../client/node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
 contract WWNFTToken is ERC721{
@@ -14,7 +14,7 @@ contract WWNFTToken is ERC721{
 
     function mintForUser(address _user) public returns(uint) {
         address user = _user;
-        // _tokenIds.increment();
+        _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(user, newItemId);
         
